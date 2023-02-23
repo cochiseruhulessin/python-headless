44e174961ce4303753ff4c350422ee5c869b4dae
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2023-02-24 00:56:01 +0100

    Add GrantType enum

--- headless/ext/oauth2/types/__init__.py
+++ headless/ext/oauth2/types/__init__.py
@@ -6,11 +6,13 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from .granttype import GrantType
 from .responsemode import ResponseMode
 from .responsetype import ResponseType
 
 
 __all__: list[str] = [
+    'GrantType',
     'ResponseType',
     'ResponseMode'
 ]
